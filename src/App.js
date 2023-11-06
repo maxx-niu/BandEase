d5a4df605eab8dcf393ddc6bd9e137ec7a6d6af9
cakec <cakecheese12@gmail.com>
2023-11-06 16:54:03 -0500

    refactor code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import './components/registration/landing-registration'
 import LandingRegistration from './components/registration/landing-registration';
 import Login from './components/login/login';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Sign Up!</NavLink>
+      </nav>
       <h1>BandEase</h1>
       <Routes>
-      <Route path="/login" element={<Login></Login>}></Route>
-      <Route path="/register" element={<LandingRegistration></LandingRegistration>}></Route>
+        <Route path="/login" element={<Login></Login>}/>
+        <Route path="/register" element={<LandingRegistration></LandingRegistration>}/>
       </Routes>
     </div>
   );
