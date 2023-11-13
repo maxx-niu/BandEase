8a7093eb50b5c74c66d8ac223d1d9a6639b7a38e
cakec <cakecheese12@gmail.com>
2023-11-13 14:24:09 -0500

    Update profile. Reformat Code

--- src/components/registration/landing-registration.js
+++ src/components/registration/landing-registration.js
@@ -1,18 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {Form, Button, Card} from 'react-bootstrap'
 import { auth } from '../../firebase'
 
 function LandingRegistration() {
 
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    // const [username, setUsername] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+
+    const emailRef = useRef();
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
 
     const handleRegistration = async (e) => {
-        e.preventDefault();
-
         // TODO: handle signup with firebase.
-        await createUserWithEmailAndPassword(auth, email, password)
+        await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
         .then((userCredential) => {
             const user = userCredential.user.email
             console.log(user);
@@ -27,44 +31,35 @@ function LandingRegistration() {
     }
 
   return (
-    <div>
-        <h2>Sign Up</h2>
-        <form onSubmit={handleRegistration}>
-            <div className='registration-input-username'>
-                <label htmlFor="username">Username: </label>
-                <input type="text" 
-                id="username" 
-                value={username}
-                required
-                onChange={(e) => {
-                    setUsername(e.target.value)
-                }}/>
-            </div>
-            <div className='registration-input-email'>
-                <label htmlFor="email">Email: </label>
-                <input type="email" 
-                id="email" 
-                value={email}
-                required
-                onChange={(e) => {
-                    setEmail(e.target.value)
-                }}/>
-            </div>
-            <div className='registration-input-password'>
-                <label htmlFor="password">Password: </label>
-                <input type="password" 
-                id="password" 
-                value={password}
-                required
-                onChange={(e) => {
-                    setPassword(e.target.value)
-                }}/>
-            </div>
-            <div className="registration-submit-button">
-                <button type="submit">Sign Up</button>
-            </div>
-        </form>
-    </div>
+    <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Sign Up</h2>
+                <Form onSubmit={handleRegistration}>
+                <Form.Group id="registration-input-email">
+                    <Form.Label htmlFor="email">Email: </Form.Label>
+                    <Form.Control id="email" type="email" required ref={emailRef}/>
+                </Form.Group>
+                <Form.Group id="registration-input-username">
+                    <Form.Label htmlFor="username">Username: </Form.Label>
+                    <Form.Control id="username" type="text" required ref={usernameRef}/>
+                </Form.Group>
+                <Form.Group id="registration-input-password">
+                    <Form.Label htmlFor="password">Password: </Form.Label>
+                    <Form.Control id="password" type="password" required ref={passwordRef}/>
+                </Form.Group>
+                <Form.Group id="registration-input-confirm-password">
+                    <Form.Label htmlFor="confirm-password">Password: </Form.Label>
+                    <Form.Control id="confirm-password" type="password" required ref={confirmPasswordRef}/>
+                </Form.Group>
+                <Button type="submit" className="w-100">Sign Up</Button>
+            </Form>
+            </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+            Already have an account? Login here
+        </div>
+    </>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -18,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 
 export const database = getDatabase(app);
 export const auth = getAuth(app);
+export default app;
