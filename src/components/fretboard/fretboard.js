e07a439b8c567fecb4aa7ed31c51770fc4fab33f
cakec <cakecheese12@gmail.com>
2024-01-03 23:34:28 -0500

    add note buttons to fretboard

--- src/components/fretboard/fretboard.js
+++ src/components/fretboard/fretboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './fretboard.css';
 import { naturalNotes, sharpNotes, flatNotes, allNotesSharp, allNotesFlat } from './notes';
+import NoteButton from './notebutton';
 
 const Fretboard = () => {
 
@@ -10,16 +11,18 @@ const Fretboard = () => {
         const fretCells = [];
         for(let j = 0; j < 14; j++){
             if(i >= 4){
+                const note = useSharps ? allNotesSharp[(5*i + j - 1) % 12] : allNotesFlat[(5*i + j - 1) % 12];
                 fretCells.push(
                     <td key={j} className="fret-cell">
-                        {useSharps ? allNotesSharp[(5*i + j - 1) % 12] : allNotesFlat[(5*i + j - 1) % 12]}
+                        <NoteButton noteName={note}></NoteButton>
                     </td>
                 )
             }
             else {
+                const note = useSharps ? allNotesSharp[(5*i + j) % 12] : allNotesFlat[(5*i + j) % 12];
                 fretCells.push(
                     <td key={j} className="fret-cell">
-                        {useSharps ? allNotesSharp[(5*i + j) % 12] : allNotesFlat[(5*i + j) % 12]}
+                        <NoteButton noteName={note}></NoteButton>
                     </td>
                 )
             } 
