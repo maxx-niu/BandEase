edd7b017ac4ce3358ec7f0342cc548f7e4ff8d43
cakec <cakecheese12@gmail.com>
2024-01-06 02:15:29 -0500

    finish fretboard ui

--- src/components/fretboard/fretboard.js
+++ src/components/fretboard/fretboard.js
@@ -7,13 +7,18 @@ const Fretboard = () => {
 
   const generateFretBoardTable = (useSharps = true) => {
     const fretRows = [];
+
+    const fretMarkers = [3, 5, 7, 9]; // Single markers
+    const doubleFretMarker = 12;
     for(let i = 0; i < 6; i++){
         const fretCells = [];
         for(let j = 0; j < 14; j++){
+            const markerClass = fretMarkers.includes(j) ? 'fret-marker' : '';
+            const doubleMarkerClass = j === doubleFretMarker ? 'double-fret-marker' : '';
             if(i >= 4){ // for B & high E strings, since G --> B is a M3, not a P4 apart
                 const note = useSharps ? allNotesSharp[(5*i + j - 1) % 12] : allNotesFlat[(5*i + j - 1) % 12];
                 fretCells.push(
-                    <td key={j} className="fret-cell">
+                    <td key={j} className={`fret-cell string-${i} fret-${j} ${markerClass} ${doubleMarkerClass}`}>
                         <NoteButton noteName={note} className="note-button"></NoteButton>
                     </td>
                 )
@@ -21,7 +26,7 @@ const Fretboard = () => {
             else {
                 const note = useSharps ? allNotesSharp[(5*i + j) % 12] : allNotesFlat[(5*i + j) % 12];
                 fretCells.push(
-                    <td key={j} className="fret-cell">
+                    <td key={j} className={`fret-cell string-${i} fret-${j} ${markerClass} ${doubleMarkerClass}`}>
                         <NoteButton noteName={note} className="note-button"></NoteButton>
                     </td>
                 )
